19d8ae125b70d3746a9c75ad766ea1d2a5ff4961
namsou <geonil87@naver.com>
2021-06-29 13:54:22 +0900

    Update ost-config.php

--- include/ost-config.php
+++ include/ost-config.php
@@ -41,7 +41,7 @@ define('ADMIN_EMAIL','demouser@opsgility.com');
 define('DBTYPE','mysql');
 define('DBHOST','user08osticketsrv01.mysql.database.azure.com');
 define('DBNAME','osticket');
-define('DBUSER','demouser@bootcampmysql999');
+define('DBUSER','demouser@user08osticketsrv01');
 define('DBPASS','demo@pass1');
 
 # Table prefix
